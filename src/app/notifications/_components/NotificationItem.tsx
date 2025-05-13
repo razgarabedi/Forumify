65630bfa26776f59f36a00e0f5b15abab12189f2
Razgar Abedi <razgar.abedi@gmail.com>
2025-05-13 07:57:54 +0000

    when i press the notification button it return this error:
    Unhandled Runtime Error
    
    Error: React is not defined

--- src/app/notifications/_components/NotificationItem.tsx
+++ src/app/notifications/_components/NotificationItem.tsx
@@ -1,5 +1,7 @@
+
 "use client";
 
+import React from 'react'; // Added React import
 import type { Notification } from '@/lib/types';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
